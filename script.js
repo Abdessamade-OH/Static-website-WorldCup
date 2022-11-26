54ca03f2ab338bb9cd16cf9185393f739dc48398
Abdessamade-OH <abdessamade.ohmad@etu.uae.ac.ma>
2022-11-26 22:28:47 +0100

    volets reduits

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function closeVol5(){
 	let vol5= document.getElementById("volet5");
 	//vol5.style.width=0;
@@ -14,5 +13,25 @@ function openVol5(){
 	vol5.style.visibility="visible";
 	let openButton = document.getElementById("openButton");
 	openButton.style.visibility="hidden";
+	console.log(width);
+}
+function closeVol2(){
+	let vol2= document.getElementById("volet2");
+	//vol5.style.width=0;
+	vol2.style.left= "-15%";
+	//vol5.style.visibility="hidden";
+	let openButton = document.getElementById("openButton2");
+	openButton.style.left= "0";
+	openButton.style.visibility="visible"
+
+}
+function openVol2(){
+	let vol2= document.getElementById("volet2");
+	//vol5.style.width= "25%";
+	vol2.style.left= "0";
+	vol2.style.visibility="visible";
+	let openButton = document.getElementById("openButton2");
+	openButton.style.visibility="hidden";
+	//openButton.style.transition ="visibility 0.5s";
 	console.log(width);
 }
