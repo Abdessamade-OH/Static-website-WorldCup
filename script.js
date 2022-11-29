2fb127baa401ed926f85023a628a4ac8a80319f9
Abdessamade-OH <abdessamade.ohmad@etu.uae.ac.ma>
2022-11-29 19:48:41 +0100

    Updated volet3

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
+var Open2 = false;
+var Open5 = true;
+var wasClosed2 = true;
+var wasClosed5=false;
+var lessThan600 = false;
+var lessThan750 = false;
 window.onload = () => {
 	console.log("Hello world!");
+	const Media750 = window.matchMedia('(max-width: 750px)');
+	checkMedia750(Media750);
+	Media750.addListener(checkMedia750);
+	
+	const Media600 = window.matchMedia('(max-width: 600px)');
+	checkMedia600(Media600);
+	Media600.addListener(checkMedia600);
+	
+	const over750 = window.matchMedia('(min-width: 750px)');
+	resetOpacity(over750);
+	over750.addListener(resetOpacity);
+	
+	
 }
 
+function checkMedia750(Media750){
+	if(Media750.matches){
+		//alert("Media query Matched!");
+		if(Open2){
+			wasClosed2=false;
+		}
+		else{
+			wasClosed2=true;
+		}
+		closeVol2();
+		lessThan750 = true;
+	}
+	else {
+		if(wasClosed2==false)
+			openVol2();
+		lessThan750 = false;
+	}
+}
+function checkMedia600(Media600){
+	if(Media600.matches){
+		if(Open5){
+			wasClosed5=false;
+		}
+		else{
+			wasClosed5=true;
+		}
+		closeVol5();
+		lessThan600 = true;
+	}
+	else {
+		if(wasClosed5==false)
+			openVol5();
+		lessThan600 = false;
+	}
+}
+function resetOpacity(over750){
+	if(over750.matches){
+		document.getElementById("field").style.opacity="1";
+	}
+}
 
 function closeVol5(){
 	let vol5= document.getElementById("volet5");
@@ -9,7 +68,12 @@ function closeVol5(){
 	vol5.style.right= "-25%";
 	//vol5.style.visibility="hidden";
 	let openButton = document.getElementById("openButton");
-	openButton.style.visibility="visible"
+	openButton.style.visibility="visible";
+	Open5 = false;
+	if(lessThan600 && Open2==false){
+		document.getElementById("field").style.opacity="1";
+	}
+	
 }
 function openVol5(){
 	let vol5= document.getElementById("volet5");
@@ -18,6 +82,10 @@ function openVol5(){
 	vol5.style.visibility="visible";
 	let openButton = document.getElementById("openButton");
 	openButton.style.visibility="hidden";
+	Open5 = true;
+	if(lessThan600 && wasClosed5==true){
+		document.getElementById("field").style.opacity="0.2";
+	}
 }
 
 function closeVol2(){
@@ -28,17 +96,25 @@ function closeVol2(){
 	let openButton = document.getElementById("openButton2");
 	openButton.style.left= "0";
 	openButton.style.visibility="visible"
-
+	Open2 = false;
+	if(lessThan600 && Open5==false){
+		document.getElementById("field").style.opacity="1";
+	}
+	if(lessThan750 && !lessThan600){
+		document.getElementById("field").style.opacity="1";
+	}
 }
 function openVol2(){
 	let vol2= document.getElementById("volet2");
-	//vol5.style.width= "25%";
 	vol2.style.left= "0";
 	vol2.style.visibility="visible";
 	let openButton = document.getElementById("openButton2");
 	openButton.style.visibility="hidden";
 	//openButton.style.transition ="visibility 0.5s";
-	console.log(width);
+	Open2 = true;
+	if(lessThan750 && wasClosed2==true){
+		document.getElementById("field").style.opacity="0.2";
+	}
 }
 
 function closeVol3(){
@@ -58,4 +134,3 @@ function openVol3(){
 	let openButton = document.getElementById("openButton3");
 	openButton.style.visibility="hidden";
 }
-
